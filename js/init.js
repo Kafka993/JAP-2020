b6df607573c8f233bc00e35419bdf6255f7b7492
Kafka993 <68866021+Kafka993@users.noreply.github.com>
2020-08-07 18:59:18 -0300

    entrega 1 parte 2
    Realicé JSON de la lista de Autos  mostrando foto, descripción, precio y autos vendidos

--- js/init.js
+++ js/init.js
@@ -43,5 +43,20 @@ var getJSONData = function(url){
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+
+
 document.addEventListener("DOMContentLoaded", function(e){
+  getJSONData(PRODUCTS_URL).then(function(resultObj){
+      if (resultObj.status === "ok")
+      {
+          productArray = resultObj.data;
+          //Muestro las categorías ordenadas
+          showProductList(productArray);
+      }
+  });
 });
+
+
+
+      
--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,9 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
