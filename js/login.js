cfc12b77263221416d2dd37c26abc52e89766d1e
Kafka993 <68866021+Kafka993@users.noreply.github.com>
2020-08-12 15:45:56 -0300

    login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,26 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+document.addEventListener("DOMContentLoaded", function(e){
+document.getElementById('submitBtn').addEventListener("click",function(e){
+    let inputEmail = document.getElementById("inputEmail");
+    let inputPassword = document.getElementById("imputPassword");
+    let camposCompletos = true;
+
+    if (inputEmail.value === ""){
+        imputEmail.classList.add("invalid")
+        camposCompletos = false;
+    
+    }
+    if( inputPassword.value === ''){
+        inputPassword.classList.add('invalid');
+        camposCompletos = false;
+    }
+
+    if(camposCompletos){
+            window.location = "index.html"
+};
+
+});
 });
