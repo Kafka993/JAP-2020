2e4caaa42d544d50f594a384c5129f8406066fbf
Kafka993 <68866021+Kafka993@users.noreply.github.com>
2020-09-09 20:40:21 -0300

    entrega 3

--- js/product-info.js
+++ js/product-info.js
@@ -4,13 +4,14 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-var  product={};
+var product = {};
 
-function showProductImg(array){
 
-    let htmlContentToAppend = "";
-    
-    for (let i = 0; i < array.length; i++ ){
+function showProductImg(array) {
+
+    let htmlContentToAppend = ""
+
+    for (let i = 0; i < array.length; i++) {
         let imageSrc = array[i];
 
         htmlContentToAppend += `
@@ -20,35 +21,77 @@ function showProductImg(array){
             </div>
         </div>
         `
-    document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
-    
     }
+
+    document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
+
 }
-document.addEventListener("DOMContentLoaded", function(){
-    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
+var commentsA = [];
+function showco(commentsA) {
+
+  
+commentsA.forEach(function (comment) {
+       
+   
+    let comments = "<hr>";
+    let score = "";
+
+        comments += ` <strong>` + comment.user + `</strong> dice:<br> <p>` + comment.description + `</p>`
         
+        for (let i = 1; i <= comment.score; i++) {
+            score += `<i class="fa fa-star checked"></i> `;
+        }
+        for (let i = comment.score + 1; i <= 5; i++) {
+            score += `<i class="fa fa-star"></i> `;
+        }
+
+        comments += `<sub>` + comment.dateTime + `</sub><br> `
+        comments += `<div style="text-align: right;">` + score + `</div><br><hr> `
+
+     
+        document.getElementById("commentsUser").innerHTML = comments;
+     
+     
+    });  
+};
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+
             product = resultObj.data;
 
-         let productNameHTML  = document.getElementById("productName");
-         let productDescriptionHTML = document.getElementById("productDescription");
-         let productCostHTML = document.getElementById("productCost");
-         let productCountHTML = document.getElementById("productCount");
-         
-     
-         productNameHTML.innerHTML = product.name;
-         productDescriptionHTML.innerHTML = product.description;
-         productCostHTML.innerHTML = product.currency +" " + product.cost;
-         productCountHTML.innerHTML = product.soldCount;
-         
+            let productNameHTML = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let productCostHTML = document.getElementById("productCost");
+            let productCountHTML = document.getElementById("productCount");
 
-         //Muestro las imagenes en forma de galería
-         showProductImg(product.images);
-     }
- });
+
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productCostHTML.innerHTML = product.currency + " " + product.cost;
+            productCountHTML.innerHTML = product.soldCount;
+
+
+            //Muestro las imagenes en forma de galería
+            showProductImg(product.images);
+        };
+    });
 });
     
-    
-     
+document.addEventListener("DOMContentLoaded", function () {
+getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+
+            commentsA = resultObj.data;
+
+            showco(commentsA)
+
+
+        }
+    });
+});
+
+
 
